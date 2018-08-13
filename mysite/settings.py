56d9da7052e053cca7d2c62a863473411f32347d
damolaajayi <damolaajayi@gmail.com>
2018-08-13 17:09:27 +0100

    Initial Commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i3)rh*%4ofsku+49znmiuar1uau-3jjo0knn(gt5&6#rtam4-3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
