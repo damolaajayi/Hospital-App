d394743149c9ce66a246484f80fb11131224c56d
damolaajayi <damolaajayi@gmail.com>
2018-08-13 16:57:10 +0100

    Initial Commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,5 +132,5 @@ STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
         ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
