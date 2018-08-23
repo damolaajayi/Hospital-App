910dd0881ff91593b003f73cb2f27cc2b540152f
damolaajayi <damolaajayi@gmail.com>
2018-08-23 09:45:59 +0100

    Initial Heroku commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,7 +132,7 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
         ]
